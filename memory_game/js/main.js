dc5b2463a7b4fcaa8b59a49251a972f0be697324
Alessandra <asangurima@gmail.com>
2019-01-24 22:39:58 -0500

    should be finalized but getting error message

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,19 +28,33 @@ var checkForMatch = function(){
 	alert("You found a match!");
 	}
 	else { alert("Sorry, try again."); }
-}
+};
 
-var flipCard = function(cardID){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 
-	console.log("User flipped " + cards[cardID].rank);
-	console.log(cards[cardID].suit);
-	console.log(cards[cardID].cardImage);
+	this.setAttribute('src',cards[cardId].cardImage);
+	
+	cardsInPlay.push(cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardID].rank);
-
- if (cardsInPlay.length === 2) {
+ if (cardsInPlay.length === 2){
  	checkForMatch();
- }}
+ 	}
+ };
 
-flipCard(0);
-flipCard(2);
+//
+var createBoard = function(){
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		var cardBoard = document.getElementById('game-board');
+		cardBoard.appendChild(cardElement);
+	}
+};
+
+createBoard();
